ad056ea8a4fae50b52e16e0ef17552ac9498c9cf
DZHURA0o0 <maltsev14022005@gmail.com>
2026-04-20 09:11:49 +0300

    ФІВАПРОЛ
    ВАПРО

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03b49a3acae70e42ad7721b1971be49e2a410d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36822b6b45d6c972ea8858cfdea7aa2a39c0c921")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
